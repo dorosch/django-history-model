4dca9d4177ffcefdf7769541a8499b6f2571550a
Andrei Doroschenko <dorosch.github.io@yandex.ru>
2020-02-28 23:01:04 +0300

    Added 'signals.py'

--- history_model/history_model/models.py
+++ history_model/history_model/models.py
@@ -3,43 +3,37 @@ import importlib
 from django.db import models
 
 from .manager import HistoryDescriptor
+from .signals import SignalDispatcher
+
+
+class HistoryModel:
+    def __init__(self, original_model):
+        self.model = original_model
+        self.bases = (models.Model,)
+        self.name = f'{self.model._meta.object_name}History'
+        self.attrs = {
+            '__module__': self.model.__module__,
+            **{field.name: field for field in self.model._meta.fields}
+        }
+
+    def create(self):
+        return type(self.name, self.bases, self.attrs)
 
 
 class HistoryRecord:
-    def contribute_to_class(self, model, name):
-        history_model = self.create_history_model(model)
+    @staticmethod
+    def contribute_to_class(original_model, name):
+        # Create history model as <ModelName>History
+        history_model = HistoryModel(original_model).create()
 
-        module = importlib.import_module(model.__module__)
+        # Replace field HistoryRecord with the historical model manager
+        descriptor = HistoryDescriptor(history_model)
+        setattr(original_model, name, descriptor)
+
+        # Register the model in module space so that migrations
+        # for the historical model can be created
+        module = importlib.import_module(original_model.__module__)
         setattr(module, history_model.__name__, history_model)
 
-        descriptor = HistoryDescriptor(history_model)
-        setattr(model, name, descriptor)
-
-        # TODO: Added class_prepared signal
-        models.signals.post_save.connect(
-            self.post_save, sender=model, weak=False
-        )
-
-    def post_save(self, instance, created, **kwargs):
-        pass
-
-    def create_history_model(self, model):
-        name = self.get_history_model_name(model)
-        bases = self.get_bases_classes()
-        attrs = self.get_attrs_history_model(model)
-        return type(name, bases, attrs)
-
-    @staticmethod
-    def get_history_model_name(model):
-        return f'{model._meta.object_name}History'
-
-    @staticmethod
-    def get_bases_classes():
-        return models.Model,
-
-    @staticmethod
-    def get_attrs_history_model(model):
-        return {
-            '__module__': model.__module__,
-            **{field.name: field for field in model._meta.fields}
-        }
+        # Register signals for records of the original model
+        SignalDispatcher.register_signals(original_model)
